383ea9d24a343702fd10e1f3952b34e5915a4788
Klyatchenok <romchikkl2001@gmail.com>
2021-05-20 16:21:35 +0300

    completed registration + created log in page

---
+++ Quizability/Controllers/LogInController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Quizability.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quizability.Controllers
+{
+    public class LogInController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public LogInController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult LogInIndex()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
