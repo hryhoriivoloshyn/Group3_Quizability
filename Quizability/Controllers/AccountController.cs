145ce018746ffc4867222b44c10fcde3c61899a6
Hryhorii-V <hryhorii.voloshyn@nure.ua>
2021-05-28 14:31:45 +0300

    Исправления ссылок

--- Quizability/Controllers/AccountController.cs
+++ Quizability/Controllers/AccountController.cs
@@ -84,7 +84,7 @@ namespace Quizability.Models
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return RedirectToAction("Login", "Account");
+            return RedirectToAction("Index", "Home");
         }
     }
 }
