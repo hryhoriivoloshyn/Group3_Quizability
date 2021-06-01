1921e092cfe2e2d9cf9e3341ec55b488fef13ba6
daywish <volodymyr.korobko@nure.ua>
2021-06-01 19:35:06 +0300

    Payment

---
+++ Quizability/Controllers/PaymentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Quizability.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quizability.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public PaymentController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        // GET: PaymentController
+        public IActionResult PremiumPayment()
+        {
+            return View();
+        }
+        public IActionResult PremiumPayment2()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
