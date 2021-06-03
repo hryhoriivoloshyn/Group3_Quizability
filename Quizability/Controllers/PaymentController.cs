ab332dad44a8f3301399cc923c061dfc0d5ee5eb
Hryhorii-V <hryhorii.voloshyn@nure.ua>
2021-06-03 16:04:46 +0300

    Оплата

--- Quizability/Controllers/PaymentController.cs
+++ Quizability/Controllers/PaymentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Quizability.Models;
@@ -10,6 +11,7 @@ using System.Threading.Tasks;
 
 namespace Quizability.Controllers
 {
+    [Authorize]
     public class PaymentController : Controller
     {
         private readonly ILogger<HomeController> _logger;
